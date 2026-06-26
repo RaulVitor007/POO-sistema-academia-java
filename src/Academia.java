cfffa25f1447f9106387dff294f9fbf78114f0e1
Raul Vitor <rvt.gc13620@gmail.com>
2026-06-25 21:03:30 -0300

    feat:POO-sistema-academia-java

---
+++ src/Academia.java
@@ -0,0 +1,12 @@
+public class Academia {
+    public static void main(String[] args){
+
+        Exercicio supino = new Exercicio("Supino Reto", 4, 12);
+        Exercicio peckDeck = new Exercicio("Peck Deck", 4, 10);
+        Exercicio supinoInclinado = new Exercicio("supino Inclinado",4,12);
+
+        supino.iniciarTreino();
+        peckDeck.iniciarTreino();
+
+    }
+}
